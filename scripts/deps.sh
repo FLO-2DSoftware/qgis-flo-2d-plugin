ff90331b8f5e9aa428a081310f97e69a98efcdc9
Peter Petrik <zilolv@gmail.com>
2016-10-03 16:12:35 +0200

    test CI

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -6,3 +6,5 @@ apt-get install spatiallite-bin
 apt-get install spatiallite-gui
 apt-get install libspatialite7
 apt-get install libgeos++-dev
+# add http://qgis.org/debian-ltr to /etc/apt/sources.list
+apt-get install qgis python-qgis qgis-plugin-grass
