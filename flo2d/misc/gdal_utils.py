2707e65ae694ed2caf973027a41b549bb72946ad
gyanz <gyanbasyalz@gmail.com>
2021-04-27 11:50:20 -0700

    Grid point elevation tool and more

---
+++ flo2d/misc/gdal_utils.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# FLO-2D Preprocessor tools for QGIS
+# Copyright © 2016 Lutra Consulting for FLO-2D
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version
+import os
+import sys
+import collections
+import math
+import warnings
+import traceback
+
+sys.path.append(os.path.dirname(__file__))
+from affine import Affine
+from transform import TransformMethodsMixin 
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    from osgeo import gdal
+    gdal.UseExceptions()
+
+class GDALRasterLayer(TransformMethodsMixin):
+    def __init__(self,raster_file):
+        self.raster_file = raster_file
+        self.ds = gdal.Open(raster_file)
+    
+    @property
+    def transform(self):
+        geotransform = self.ds.GetGeoTransform()
+        return Affine.from_gdal(*geotransform)
+    
