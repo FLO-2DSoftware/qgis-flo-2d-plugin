bbeed3c1dbaec75a539191335d4e87968998974f
lukasz.debek <lukasz.debek@lutraconsulting.co.uk>
2021-06-25 11:44:34 +0200

    Minor code changes.

--- docs/user/source/conf.py
+++ docs/user/source/conf.py
@@ -13,48 +13,40 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('...'))
+sys.path.insert(0, os.path.abspath("..."))
 
 # -- Project information -----------------------------------------------------
 
-project = 'FLO-2D-Tutorials'
+project = "FLO-2D-Tutorials"
 copyright = "2021, Karen O'Brien"
 author = "Karen O'Brien"
-html_logo = 'Flo2D_logo.jpg'
+html_logo = "Flo2D_logo.jpg"
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.yt'
-]
+extensions = ["sphinxcontrib.yt"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['style']
+templates_path = ["style"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
-#html_theme_path = ['style']
-html_static_path = ['style']
-html_css_files = ['static/style.css']
-
-
-
-
-
-
+html_theme = "sphinx_book_theme"
+# html_theme_path = ["style"]
+html_static_path = ["style"]
+html_css_files = ["static/style.css"]
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-
