3ee465cea42e07409ea42aa866ace181834144d0
JJ <jj@flo-2d.com>
2021-12-04 16:53:48 -0400

    Fix auto assign of inlets/outlet names to conduits

--- flo2d/gui/storm_drain_editor_widget.py
+++ flo2d/gui/storm_drain_editor_widget.py
@@ -2281,7 +2281,7 @@ class StormDrainEditorWidget(qtBaseClass, uiDialog):
             self.user_swmm_conduits_lyr.triggerRepaint()
             QApplication.restoreOverrideCursor()
             self.uc.show_info(
-                "Inlet and Outlet node names successfully assigned for the {len(conduit_nodes)} Conduits!"
+                "Inlet and Outlet node names successfully assigned to " + len(conduit_nodes) + " Conduits!"
             )
         except Exception as e:
             QApplication.restoreOverrideCursor()
