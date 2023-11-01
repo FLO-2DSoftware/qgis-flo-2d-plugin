5d5171fd35cb0179484faf1bf7436d5379073c43
Karen <karen@flo-2d.com>
2023-11-01 16:25:22 -0500

    contrib_yt removal (#1014)
    * Update requirements.txt
    
    * Update conf.py (#1013)

--- docs/user/source/conf.py
+++ docs/user/source/conf.py
@@ -26,7 +26,7 @@ html_logo = "Flo2D_logo.jpg"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinxcontrib.yt"]
+# extensions = ["sphinxcontrib.yt"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["style"]
