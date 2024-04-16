6e3fab0acad0f876c62a8f4a472f875c68bd74d8
Robson Pachaly <39889306+rpachaly@users.noreply.github.com>
2024-04-16 07:35:40 -0300

    Added a help button on the Grid Tools (#1273)

--- flo2d/gui/grid_tools_widget.py
+++ flo2d/gui/grid_tools_widget.py
@@ -12,6 +12,8 @@ import os
 import time
 import traceback
 
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
 from qgis.PyQt import QtCore, QtGui
 from qgis.core import NULL, Qgis, QgsFeature, QgsGeometry, QgsMessageLog, QgsWkbTypes
 from qgis.PyQt import QtWidgets
@@ -109,6 +111,7 @@ class GridToolsWidget(qtBaseClass, uiDialog):
         self.noexchange_btn.clicked.connect(self.eval_noexchange)
         self.other_variable_btn.clicked.connect(self.other_variable)
         self.tailings_btn.clicked.connect(self.get_tailings)
+        self.help_btn.clicked.connect(self.show_grid_widget_help)
 
     def setup_connection(self):
         con = self.iface.f2d["con"]
@@ -1321,3 +1324,9 @@ class GridToolsWidget(qtBaseClass, uiDialog):
                 "WARNING 060319.1721: Selection of no-exchange cells failed! Please check your No-xchange Cells (Tables layer)."
             )
             QApplication.restoreOverrideCursor()
+
+    def show_grid_widget_help(self):
+        """
+        Function to show the grid widget help
+        """
+        QDesktopServices.openUrl(QUrl("https://flo-2dsoftware.github.io/FLO-2D-Documentation/Plugin1000/widgets/grid-tools/index.html"))
