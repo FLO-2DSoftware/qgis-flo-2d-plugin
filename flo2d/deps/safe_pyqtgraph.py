15a249c2874066e27a552175334a1cf471cadc01
rpachaly <39889306+rpachaly@users.noreply.github.com>
2024-05-25 15:07:58 -0300

    Changed the pyqtgraph version to a more updated (13.3->13.7)

--- flo2d/deps/safe_pyqtgraph.py
+++ flo2d/deps/safe_pyqtgraph.py
@@ -5,5 +5,5 @@ try:
 except ImportError:
     from .. import utils
 
-    utils.add_egg_or_wheel("pyqtgraph-0.13.3-py3-none-any.whl")
+    utils.add_egg_or_wheel("pyqtgraph-0.13.7-py3-none-any.whl")
     from pyqtgraph import *
