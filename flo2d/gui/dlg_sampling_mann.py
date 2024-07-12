966d58e9ae1b9a85055b4a8495927287cbe0e033
Robson Pachaly <39889306+rpachaly@users.noreply.github.com>
2024-07-12 11:50:31 -0300

    Force a layer reload when opening the Sampling Manning (#1390)

--- flo2d/gui/dlg_sampling_mann.py
+++ flo2d/gui/dlg_sampling_mann.py
@@ -42,6 +42,7 @@ class SamplingManningDialog(qtBaseClass, uiDialog):
         poly_lyrs = self.lyrs.list_group_vlayers()
         for l in poly_lyrs:
             if l.geometryType() == QgsWkbTypes.PolygonGeometry:
+                l.reload()  # force layer reload because sometimes featureCount does not work
                 if l.featureCount() > 0:
                     self.srcLayerCbo.addItem(l.name(), l.dataProvider().dataSourceUri())
             else:
