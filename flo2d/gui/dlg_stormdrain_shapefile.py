2b1b46c3c06225fa9d2571664b0f31f2d734d2a2
rpachaly <39889306+rpachaly@users.noreply.github.com>
2024-07-25 10:43:28 -0300

    Removed storm drain control variables after assign shapefiles is clicked
    Smart code to load True/False from old allowQ shapefiles

--- flo2d/gui/dlg_stormdrain_shapefile.py
+++ flo2d/gui/dlg_stormdrain_shapefile.py
@@ -1486,6 +1486,7 @@ class StormDrainShapefile(qtBaseClass, uiDialog):
                         if self.outfall_allow_discharge_FieldCbo.currentText() != ""
                         else ""
                     )
+                    swmm_allow_discharge = 1 if swmm_allow_discharge == 'True' else 0
                     swmm_allow_discharge = str(swmm_allow_discharge) if swmm_allow_discharge in ["0", "1", "2", 0, 1, 2] else "0"
                     
                     outfall_type = (
