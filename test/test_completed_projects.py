a5520f76e83bc6e990ad4e7a440dedd01657c866
rpachaly <39889306+rpachaly@users.noreply.github.com>
2024-08-16 14:55:46 -0300

    Added a check for file

--- test/test_completed_projects.py
+++ test/test_completed_projects.py
@@ -61,164 +61,191 @@ class TestCompletedProjects(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.con.close()
-        # for f in os.listdir(cls.export_folder):
-        #     fpath = os.path.join(cls.export_folder, f)
-        #     if os.path.isfile(fpath):
-        #         os.remove(fpath)
-        #     else:
-        #         pass
-        # os.rmdir(cls.export_folder)
+        for f in os.listdir(cls.export_folder):
+            fpath = os.path.join(cls.export_folder, f)
+            if os.path.isfile(fpath):
+                os.remove(fpath)
+            else:
+                pass
+        os.rmdir(cls.export_folder)
 
     @unittest.skip("Skipping test to fix the issue.")
     def test_arf(self):
-
-        self.f2g.import_arf()
-        self.f2g.export_arf(self.export_folder)
-        infile = self.f2g.parser.dat_files["ARF.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\ARF.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_arf()
+            self.f2g.export_arf(self.export_folder)
+            infile = self.f2g.parser.dat_files["ARF.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_chan(self):
-        self.f2g.import_chan()
-        self.f2g.export_chan(self.export_folder)
-        infile1 = self.f2g.parser.dat_files["CHAN.DAT"]
-        infile2 = self.f2g.parser.dat_files["CHANBANK.DAT"]
-        outfile1, outfile2 = export_paths(self.export_folder, infile1, infile2)
-        in_lines1, out_lines1 = compare_files(infile1, outfile1)
-        self.assertEqual(in_lines1, out_lines1)
-        in_lines2, out_lines2 = compare_files(infile2, outfile2)
-        self.assertEqual(in_lines2, out_lines2)
+        file1 = self.import_folder + r"\CHAN.DAT"
+        file2 = self.import_folder + r"\CHANBANK.DAT"
+        if os.path.isfile(file1) and os.path.isfile(file2):
+            self.f2g.import_chan()
+            self.f2g.export_chan(self.export_folder)
+            infile1 = self.f2g.parser.dat_files["CHAN.DAT"]
+            infile2 = self.f2g.parser.dat_files["CHANBANK.DAT"]
+            outfile1, outfile2 = export_paths(self.export_folder, infile1, infile2)
+            in_lines1, out_lines1 = compare_files(infile1, outfile1)
+            self.assertEqual(in_lines1, out_lines1)
+            in_lines2, out_lines2 = compare_files(infile2, outfile2)
+            self.assertEqual(in_lines2, out_lines2)
 
     def test_cont_toler(self):
-
-        self.f2g.import_cont_toler()
-        self.f2g.export_cont_toler(self.export_folder)
-        infile1 = self.f2g.parser.dat_files["CONT.DAT"]
-        infile2 = self.f2g.parser.dat_files["TOLER.DAT"]
-        outfile1, outfile2 = export_paths(self.export_folder, infile1, infile2)
-        in_lines1, out_lines1 = compare_files(infile1, outfile1)
-        self.assertEqual(in_lines1, out_lines1)
-        in_lines2, out_lines2 = compare_files(infile2, outfile2)
-        self.assertEqual(in_lines2, out_lines2)
+        file1 = self.import_folder + r"\CONT.DAT"
+        file2 = self.import_folder + r"\TOLER.DAT"
+        if os.path.isfile(file1) and os.path.isfile(file2):
+            self.f2g.import_cont_toler()
+            self.f2g.export_cont_toler(self.export_folder)
+            infile1 = self.f2g.parser.dat_files["CONT.DAT"]
+            infile2 = self.f2g.parser.dat_files["TOLER.DAT"]
+            outfile1, outfile2 = export_paths(self.export_folder, infile1, infile2)
+            in_lines1, out_lines1 = compare_files(infile1, outfile1)
+            self.assertEqual(in_lines1, out_lines1)
+            in_lines2, out_lines2 = compare_files(infile2, outfile2)
+            self.assertEqual(in_lines2, out_lines2)
 
     def test_hystruct(self):
 
-        self.f2g.import_hystruc()
-        self.f2g.export_hystruc(self.export_folder)
-        infile = self.f2g.parser.dat_files["HYSTRUC.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\HYSTRUC.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_hystruc()
+            self.f2g.export_hystruc(self.export_folder)
+            infile = self.f2g.parser.dat_files["HYSTRUC.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_infil(self):
-        self.f2g.import_infil()
-        self.f2g.export_infil(self.export_folder)
-        infile = self.f2g.parser.dat_files["INFIL.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\INFIL.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_infil()
+            self.f2g.export_infil(self.export_folder)
+            infile = self.f2g.parser.dat_files["INFIL.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_inflow(self):
-        self.f2g.import_inflow()
-        self.f2g.export_inflow(self.export_folder)
-        infile = self.f2g.parser.dat_files["INFLOW.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\INFLOW.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_inflow()
+            self.f2g.export_inflow(self.export_folder)
+            infile = self.f2g.parser.dat_files["INFLOW.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_levee(self):
-        self.f2g.import_levee()
-        self.f2g.export_levee(self.export_folder)
-        infile = self.f2g.parser.dat_files["LEVEE.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\LEVEE.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_levee()
+            self.f2g.export_levee(self.export_folder)
+            infile = self.f2g.parser.dat_files["LEVEE.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_manning_n_topo(self):
-
-        self.f2g.import_mannings_n_topo()
-        self.f2g.export_mannings_n_topo(self.export_folder)
-        infile1 = self.f2g.parser.dat_files["MANNINGS_N.DAT"]
-        infile2 = self.f2g.parser.dat_files["TOPO.DAT"]
-        outfile1, outfile2 = export_paths(self.export_folder, infile1, infile2)
-        in_lines1, out_lines1 = compare_files(infile1, outfile1)
-        self.assertEqual(in_lines1, out_lines1)
-        in_lines2, out_lines2 = compare_files(infile2, outfile2)
-        self.assertEqual(in_lines2, out_lines2)
+        file1 = self.import_folder + r"\MANNINGS_N.DAT"
+        file2 = self.import_folder + r"\TOPO.DAT"
+        if os.path.isfile(file1) and os.path.isfile(file2):
+            self.f2g.import_mannings_n_topo()
+            self.f2g.export_mannings_n_topo(self.export_folder)
+            infile1 = self.f2g.parser.dat_files["MANNINGS_N.DAT"]
+            infile2 = self.f2g.parser.dat_files["TOPO.DAT"]
+            outfile1, outfile2 = export_paths(self.export_folder, infile1, infile2)
+            in_lines1, out_lines1 = compare_files(infile1, outfile1)
+            self.assertEqual(in_lines1, out_lines1)
+            in_lines2, out_lines2 = compare_files(infile2, outfile2)
+            self.assertEqual(in_lines2, out_lines2)
 
     def test_outflow(self):
-        self.f2g.import_outflow()
-        self.f2g.export_outflow(self.export_folder)
-        infile = self.f2g.parser.dat_files["OUTFLOW.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\OUTFLOW.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_outflow()
+            self.f2g.export_outflow(self.export_folder)
+            infile = self.f2g.parser.dat_files["OUTFLOW.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_rain(self):
-        self.f2g.import_rain()
-        self.f2g.export_rain(self.export_folder)
-        infile = self.f2g.parser.dat_files["RAIN.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\RAIN.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_rain()
+            self.f2g.export_rain(self.export_folder)
+            infile = self.f2g.parser.dat_files["RAIN.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     @unittest.skip("Skipping test to fix the issue.")
     def test_sdclogging(self):
-
-        self.f2g.import_sdclogging()
-        self.f2g.export_sdclogging(self.export_folder)
-        infile = self.f2g.parser.dat_files["SDCLOGGING.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\SDCLOGGING.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_sdclogging()
+            self.f2g.export_sdclogging(self.export_folder)
+            infile = self.f2g.parser.dat_files["SDCLOGGING.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_swmmflo(self):
-
-        self.f2g.import_swmmflo()
-        self.f2g.export_swmmflo(self.export_folder)
-        infile = self.f2g.parser.dat_files["SWMMFLO.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\SWMMFLO.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_swmmflo()
+            self.f2g.export_swmmflo(self.export_folder)
+            infile = self.f2g.parser.dat_files["SWMMFLO.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     @unittest.skip("Skipping test to fix the issue.")
     def test_swmmflodropbox(self):
-        self.f2g.import_swmmflodropbox()
-        self.f2g.export_swmmflodropbox(self.export_folder)
-        infile = self.f2g.parser.dat_files["SWMMFLODROPBOX.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\SWMMFLODROPBOX.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_swmmflodropbox()
+            self.f2g.export_swmmflodropbox(self.export_folder)
+            infile = self.f2g.parser.dat_files["SWMMFLODROPBOX.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     @unittest.skip("Skipping test to fix the issue.")
     def test_swmmflort(self):
-
-        self.f2g.import_swmmflort()
-        self.f2g.export_swmmflort(self.export_folder)
-        infile = self.f2g.parser.dat_files["SWMMFLORT.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\SWMMFLORT.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_swmmflort()
+            self.f2g.export_swmmflort(self.export_folder)
+            infile = self.f2g.parser.dat_files["SWMMFLORT.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     @unittest.skip("Skipping test to fix the issue.")
     def test_swmmoutf(self):  # TODO IMPROVE THIS CODE FOR WHEN WE HAVE -9999
-
-        self.f2g.import_swmmoutf()
-        self.f2g.export_swmmoutf(self.export_folder)
-        infile = self.f2g.parser.dat_files["SWMMOUTF.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\SWMMOUTF.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_swmmoutf()
+            self.f2g.export_swmmoutf(self.export_folder)
+            infile = self.f2g.parser.dat_files["SWMMOUTF.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
     def test_xsec(self):
-
-        self.f2g.import_xsec()
-        self.f2g.export_xsec(self.export_folder)
-        infile = self.f2g.parser.dat_files["XSEC.DAT"]
-        outfile = export_paths(self.export_folder, infile)
-        in_lines, out_lines = compare_files(infile, outfile)
-        self.assertEqual(in_lines, out_lines)
+        file = self.import_folder + r"\XSEC.DAT"
+        if os.path.isfile(file):
+            self.f2g.import_xsec()
+            self.f2g.export_xsec(self.export_folder)
+            infile = self.f2g.parser.dat_files["XSEC.DAT"]
+            outfile = export_paths(self.export_folder, infile)
+            in_lines, out_lines = compare_files(infile, outfile)
+            self.assertEqual(in_lines, out_lines)
 
 
 if __name__ == "__main__":
