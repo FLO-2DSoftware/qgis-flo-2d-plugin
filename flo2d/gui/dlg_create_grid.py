8cd698b6e839cc3cac144d0f9ab5d2d1869ebda7
rpachaly <39889306+rpachaly@users.noreply.github.com>
2025-01-08 16:08:14 -0300

    Added a filter to the external layer on the create grid dialog that only accepts polygon layers with one feature on it.

--- flo2d/gui/dlg_create_grid.py
+++ flo2d/gui/dlg_create_grid.py
@@ -11,6 +11,7 @@
 
 import os
 
+from qgis._core import QgsWkbTypes
 from qgis.core import QgsFieldProxyModel, QgsMapLayerProxyModel
 from qgis.PyQt.QtCore import QSettings
 from qgis.PyQt.QtWidgets import QFileDialog
@@ -21,23 +22,44 @@ uiDialog, qtBaseClass = load_ui("create_grid")
 
 
 class CreateGridDialog(qtBaseClass, uiDialog):
-    def __init__(self):
+    def __init__(self, lyrs):
         qtBaseClass.__init__(self)
         uiDialog.__init__(self)
+        self.lyrs = lyrs
         self.setupUi(self)
-        self.external_lyr_cbo.setFilters(QgsMapLayerProxyModel.PolygonLayer)
         self.cell_size_cbo.setFilters(QgsFieldProxyModel.Numeric | QgsFieldProxyModel.Int)
-        self.external_layer_changed()
+        self.current_lyr = None
         self.method_changed()
+
         # connections
-        self.external_lyr_cbo.currentIndexChanged.connect(self.external_layer_changed)
+        self.setup_src_layer_cbo()
+
+        self.external_lyr_cbo.currentIndexChanged.connect(self.populate_src_field_cbo)
         self.use_external_lyr_rb.toggled.connect(self.method_changed)
         self.use_user_layer_rb.setChecked(True)
         self.browseBtn.clicked.connect(self.browse_raster_file)
 
-    def external_layer_changed(self):
-        current_layer = self.external_lyr_cbo.currentLayer()
-        self.cell_size_cbo.setLayer(current_layer)
+    def setup_src_layer_cbo(self):
+        """
+        Filter src layer combo for polygons and connect field cbo.
+        """
+        self.external_lyr_cbo.addItem("", None)
+        poly_lyrs = self.lyrs.list_group_vlayers()
+        for l in poly_lyrs:
+            if l.geometryType() == QgsWkbTypes.PolygonGeometry:
+                l.reload()  # force layer reload because sometimes featureCount does not work
+                if l.featureCount() == 1:
+                    self.external_lyr_cbo.addItem(l.name(), l.dataProvider().dataSourceUri())
+            else:
+                pass
+
+    def populate_src_field_cbo(self, idx):
+        if idx == 0:
+            return
+        uri = self.external_lyr_cbo.itemData(idx)
+        lyr_id = self.lyrs.layer_exists_in_group(uri)
+        self.current_lyr = self.lyrs.get_layer_tree_item(lyr_id).layer()
+        self.cell_size_cbo.setLayer(self.current_lyr)
 
     def method_changed(self):
         if self.use_external_lyr_rb.isChecked():
@@ -49,7 +71,7 @@ class CreateGridDialog(qtBaseClass, uiDialog):
         return self.use_external_lyr_rb.isChecked()
 
     def external_layer_parameters(self):
-        current_layer = self.external_lyr_cbo.currentLayer()
+        current_layer = self.lyrs.get_layer_by_name(self.external_lyr_cbo.currentText()).layer()
         cell_size_field = self.cell_size_cbo.currentField()
         raster_file = self.raster_file_lab.text().strip()
         return current_layer, cell_size_field, raster_file
