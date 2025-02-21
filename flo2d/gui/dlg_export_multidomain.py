2d3814940704bb98f789a705ea9d97504b46d10a
rpachaly <39889306+rpachaly@users.noreply.github.com>
2025-02-21 14:27:34 -0300

    Export process done

--- flo2d/gui/dlg_export_multidomain.py
+++ flo2d/gui/dlg_export_multidomain.py
@@ -3,6 +3,7 @@ import shutil
 
 from PyQt5.QtCore import QSettings, Qt
 from PyQt5.QtWidgets import QFileDialog, QApplication, QCheckBox
+from pyodbc import connect
 from qgis.PyQt.QtCore import NULL
 
 from .ui_utils import load_ui
@@ -78,6 +79,14 @@ class ExportMultipleDomainsDialog(qtBaseClass, uiDialog):
         Function to export the subdomains to different folders
         """
         QApplication.setOverrideCursor(Qt.WaitCursor)
+
+        # Figure out the export method
+        # 0 -> MULTIDOMAIN.DAT
+        # 1 -> CADPTS_DSx.DAT
+        # 2 -> NO CONNECTIVITY
+        # 3 -> ONLY MULTIDOMAIN.DAT
+        # 4 -> ONLY CADPTS_DSx.DAT
+        export_method = self.export_method_cbo.currentIndex()
 
         export_subdomains = []
 
@@ -156,40 +165,137 @@ class ExportMultipleDomainsDialog(qtBaseClass, uiDialog):
 
                     records = sorted(sub_grid_cells, key=lambda x: x[0])
 
-                # try:
                 mannings = os.path.join(str(export_folder), "MANNINGS_N.DAT")
                 topo = os.path.join(str(export_folder), "TOPO.DAT")
                 cadpts = os.path.join(str(export_folder), "CADPTS.DAT")
+                multidomain = os.path.join(str(export_folder), "MULTIDOMAIN.DAT")
+
+                nulls = 0
 
                 mline = "{0: >10} {1: >10}\n"
                 tline = "{0: >15} {1: >15} {2: >10}\n"
                 cline = "{0: >9} {1: >14} {2: >14}\n"
-                nulls = 0
-                with open(mannings, "w") as m, open(topo, "w") as t, open(cadpts, "w") as c:
-                    for row in records:
-                        fid, man, elev, geom = row
-                        if man == None or elev == None:
-                            nulls += 1
-                            if man == None:
-                                man = 0.04
-                            if elev == None:
-                                elev = -9999
-                        x, y = geom.strip("POINT()").split()
-                        m.write(mline.format(fid, "{0:.3f}".format(man)))
-                        t.write(
-                            tline.format(
-                                "{0:.4f}".format(float(x)),
-                                "{0:.4f}".format(float(y)),
-                                "{0:.4f}".format(elev),
+                mdline_n = "N {0}\n"
+                mdline_d = "D {0} {1}\n"
+
+                # MULTIDOMAIN.DAT
+                if export_method == 0:
+                    with open(mannings, "w") as m, open(topo, "w") as t:
+                        for row in records:
+                            fid, man, elev, geom = row
+                            if man == None or elev == None:
+                                nulls += 1
+                                if man == None:
+                                    man = 0.04
+                                if elev == None:
+                                    elev = -9999
+                            x, y = geom.strip("POINT()").split()
+                            m.write(mline.format(fid, "{0:.3f}".format(man)))
+                            t.write(
+                                tline.format(
+                                    "{0:.4f}".format(float(x)),
+                                    "{0:.4f}".format(float(y)),
+                                    "{0:.4f}".format(elev),
+                                )
                             )
-                        )
-                        c.write(
-                            cline.format(
-                                fid,
-                                "{0:.3f}".format(float(x)),
-                                "{0:.3f}".format(float(y))
+                    connected_subdomains = self.gutils.execute(
+                        f"""SELECT DISTINCT down_domain_fid FROM schema_md_connect_cells WHERE up_domain_fid = {subdomains[0]};""").fetchall()
+                    if connected_subdomains:
+                        with open(multidomain, "w") as md:
+                            for i, connected_subdomain in enumerate(connected_subdomains, start=1):
+                                md.write(mdline_n.format(i))
+                                connections = self.gutils.execute(
+                                    f"""SELECT up_domain_cell, down_domain_cell FROM schema_md_connect_cells WHERE up_domain_fid = {subdomains[0]} and down_domain_fid = {connected_subdomain[0]};""").fetchall()
+                                if connections:
+                                    connection_dict = {}
+                                    for up_domain_cell, down_domain_cell in connections:
+                                        if up_domain_cell not in connection_dict:
+                                            connection_dict[up_domain_cell] = []
+                                        connection_dict[up_domain_cell].append(down_domain_cell)
+
+                                    for up_domain_cell, down_domain_cells in connection_dict.items():
+                                        md.write(mdline_d.format(up_domain_cell, " ".join(map(str, down_domain_cells))))
+
+                # CADPTS_DSx.DAT
+                elif export_method == 1:
+                    with open(mannings, "w") as m, open(topo, "w") as t, open(cadpts, "w") as c:
+                        for row in records:
+                            fid, man, elev, geom = row
+                            if man == None or elev == None:
+                                nulls += 1
+                                if man == None:
+                                    man = 0.04
+                                if elev == None:
+                                    elev = -9999
+                            x, y = geom.strip("POINT()").split()
+                            m.write(mline.format(fid, "{0:.3f}".format(man)))
+                            t.write(
+                                tline.format(
+                                    "{0:.4f}".format(float(x)),
+                                    "{0:.4f}".format(float(y)),
+                                    "{0:.4f}".format(elev),
+                                )
                             )
-                        )
+                            c.write(
+                                cline.format(
+                                    fid,
+                                    "{0:.3f}".format(float(x)),
+                                    "{0:.3f}".format(float(y))
+                                )
+                            )
+                # NO CONNECTIVITY
+                elif export_method == 2:
+                    with open(mannings, "w") as m, open(topo, "w") as t:
+                        for row in records:
+                            fid, man, elev, geom = row
+                            if man == None or elev == None:
+                                nulls += 1
+                                if man == None:
+                                    man = 0.04
+                                if elev == None:
+                                    elev = -9999
+                            x, y = geom.strip("POINT()").split()
+                            m.write(mline.format(fid, "{0:.3f}".format(man)))
+                            t.write(
+                                tline.format(
+                                    "{0:.4f}".format(float(x)),
+                                    "{0:.4f}".format(float(y)),
+                                    "{0:.4f}".format(elev),
+                                )
+                            )
+                # ONLY MULTIDOMAIN.DAT
+                elif export_method == 3:
+                    connected_subdomains = self.gutils.execute(
+                        f"""SELECT DISTINCT down_domain_fid FROM schema_md_connect_cells WHERE up_domain_fid = {subdomains[0]};""").fetchall()
+                    if connected_subdomains:
+                        with open(multidomain, "w") as md:
+                            for i, connected_subdomain in enumerate(connected_subdomains, start=1):
+                                md.write(mdline_n.format(i))
+                                connections = self.gutils.execute(
+                                    f"""SELECT up_domain_cell, down_domain_cell FROM schema_md_connect_cells WHERE up_domain_fid = {subdomains[0]} and down_domain_fid = {connected_subdomain[0]};""").fetchall()
+                                if connections:
+                                    connection_dict = {}
+                                    for up_domain_cell, down_domain_cell in connections:
+                                        if up_domain_cell not in connection_dict:
+                                            connection_dict[up_domain_cell] = []
+                                        connection_dict[up_domain_cell].append(down_domain_cell)
+
+                                    for up_domain_cell, down_domain_cells in connection_dict.items():
+                                        md.write(mdline_d.format(up_domain_cell, " ".join(map(str, down_domain_cells))))
+
+                # ONLY CADPTS_DSx.DAT
+                elif export_method == 4:
+                    with open(cadpts, "w") as c:
+                        for row in records:
+                            fid, _, _, geom = row
+                            x, y = geom.strip("POINT()").split()
+                            c.write(
+                                cline.format(
+                                    fid,
+                                    "{0:.3f}".format(float(x)),
+                                    "{0:.3f}".format(float(y))
+                                )
+                            )
 
                 if nulls > 0:
                     QApplication.restoreOverrideCursor()
@@ -206,6 +312,37 @@ class ExportMultipleDomainsDialog(qtBaseClass, uiDialog):
                 #     QApplication.restoreOverrideCursor()
                 #     self.uc.show_error("ERROR 101218.1541: exporting MANNINGS_N.DAT or TOPO.DAT failed!.\n", e)
                 #     QApplication.setOverrideCursor(Qt.WaitCursor)
+
+            # Copy and rename the CADPTS to CADPTS_DSx to the correct folders
+            if export_method in [1, 4]:
+                subdomain_connectivities_names = self.gutils.execute(f"""
+                                                   SELECT
+                                                       im.subdomain_name,
+                                                       im.subdomain_name_1,
+                                                       im.subdomain_name_2,
+                                                       im.subdomain_name_3,
+                                                       im.subdomain_name_4,
+                                                       im.subdomain_name_5,
+                                                       im.subdomain_name_6,
+                                                       im.subdomain_name_7,
+                                                       im.subdomain_name_8,
+                                                       im.subdomain_name_9
+                                                   FROM
+                                                       mult_domains_con AS im;
+                                                               """).fetchall()
+                if subdomain_connectivities_names:
+                    for subdomain_connectivity_name in subdomain_connectivities_names:
+                        current_subdomain = subdomain_connectivity_name[0]
+                        current_subdomain_folder = os.path.join(self.export_directory_le.text(), current_subdomain)
+                        for i in range(1, 10):
+                            downstream_subdomains_folder = os.path.join(self.export_directory_le.text(),
+                                                                        subdomain_connectivity_name[i])
+                            if not os.path.exists(downstream_subdomains_folder) or subdomain_connectivity_name[
+                                i] == "":
+                                continue
+                            else:
+                                shutil.copy2(os.path.join(str(downstream_subdomains_folder), "CADPTS.DAT"),
+                                             os.path.join(str(current_subdomain_folder), f"CADPTS_DS{i}.DAT"))
 
         QApplication.restoreOverrideCursor()
         self.uc.log_info(f"Subdomains exported correctly!")
@@ -292,16 +429,54 @@ class ExportMultipleDomainsDialog(qtBaseClass, uiDialog):
                 mannings = os.path.join(str(export_folder), "MANNINGS_N.DAT")
                 topo = os.path.join(str(export_folder), "TOPO.DAT")
                 cadpts = os.path.join(str(export_folder), "CADPTS.DAT")
+                multidomain = os.path.join(str(export_folder), "MULTIDOMAIN.DAT")
 
                 nulls = 0
 
                 mline = "{0: >10} {1: >10}\n"
                 tline = "{0: >15} {1: >15} {2: >10}\n"
                 cline = "{0: >9} {1: >14} {2: >14}\n"
+                mdline_n = "N {0}\n"
+                mdline_d = "D {0} {1}\n"
 
                 # MULTIDOMAIN.DAT
                 if export_method == 0:
-                    pass
+                    with open(mannings, "w") as m, open(topo, "w") as t:
+                        for row in records:
+                            fid, man, elev, geom = row
+                            if man == None or elev == None:
+                                nulls += 1
+                                if man == None:
+                                    man = 0.04
+                                if elev == None:
+                                    elev = -9999
+                            x, y = geom.strip("POINT()").split()
+                            m.write(mline.format(fid, "{0:.3f}".format(man)))
+                            t.write(
+                                tline.format(
+                                    "{0:.4f}".format(float(x)),
+                                    "{0:.4f}".format(float(y)),
+                                    "{0:.4f}".format(elev),
+                                )
+                            )
+                    connected_subdomains = self.gutils.execute(
+                        f"""SELECT DISTINCT down_domain_fid FROM schema_md_connect_cells WHERE up_domain_fid = {subdomain[0]};""").fetchall()
+                    if connected_subdomains:
+                        with open(multidomain, "w") as md:
+                            for i, connected_subdomain in enumerate(connected_subdomains, start=1):
+                                md.write(mdline_n.format(i))
+                                connections = self.gutils.execute(
+                                    f"""SELECT up_domain_cell, down_domain_cell FROM schema_md_connect_cells WHERE up_domain_fid = {subdomain[0]} and down_domain_fid = {connected_subdomain[0]};""").fetchall()
+                                if connections:
+                                    connection_dict = {}
+                                    for up_domain_cell, down_domain_cell in connections:
+                                        if up_domain_cell not in connection_dict:
+                                            connection_dict[up_domain_cell] = []
+                                        connection_dict[up_domain_cell].append(down_domain_cell)
+
+                                    for up_domain_cell, down_domain_cells in connection_dict.items():
+                                        md.write(mdline_d.format(up_domain_cell, " ".join(map(str, down_domain_cells))))
+
                 # CADPTS_DSx.DAT
                 elif export_method == 1:
                     with open(mannings, "w") as m, open(topo, "w") as t, open(cadpts, "w") as c:
@@ -351,7 +526,24 @@ class ExportMultipleDomainsDialog(qtBaseClass, uiDialog):
                             )
                 # ONLY MULTIDOMAIN.DAT
                 elif export_method == 3:
-                    pass
+                    connected_subdomains = self.gutils.execute(
+                        f"""SELECT DISTINCT down_domain_fid FROM schema_md_connect_cells WHERE up_domain_fid = {subdomain[0]};""").fetchall()
+                    if connected_subdomains:
+                        with open(multidomain, "w") as md:
+                            for i, connected_subdomain in enumerate(connected_subdomains, start=1):
+                                md.write(mdline_n.format(i))
+                                connections = self.gutils.execute(
+                                    f"""SELECT up_domain_cell, down_domain_cell FROM schema_md_connect_cells WHERE up_domain_fid = {subdomain[0]} and down_domain_fid = {connected_subdomain[0]};""").fetchall()
+                                if connections:
+                                    connection_dict = {}
+                                    for up_domain_cell, down_domain_cell in connections:
+                                        if up_domain_cell not in connection_dict:
+                                            connection_dict[up_domain_cell] = []
+                                        connection_dict[up_domain_cell].append(down_domain_cell)
+
+                                    for up_domain_cell, down_domain_cells in connection_dict.items():
+                                        md.write(mdline_d.format(up_domain_cell, " ".join(map(str, down_domain_cells))))
+
                 # ONLY CADPTS_DSx.DAT
                 elif export_method == 4:
                     with open(cadpts, "w") as c:
