54fe4ae740c7f9463bf0ba3024d8613d581f4c90
Robson Pachaly <39889306+rpachaly@users.noreply.github.com>
2025-09-09 09:51:27 -0300

    Add try-except blocks for error handling in rainfall data import and restore cursor state (#1858)

--- flo2d/flo2d_ie/rainfall_io.py
+++ flo2d/flo2d_ie/rainfall_io.py
@@ -11,22 +11,12 @@ import os
 
 import numpy as np
 
-try:
-    from netCDF4 import Dataset, num2date
-except ImportError:
-    pass
-
 from qgis._core import QgsCoordinateTransform, QgsProject, QgsCoordinateReferenceSystem, QgsPointXY
 
 from ..flo2d_tools.grid_tools import rasters2centroids
 from ..geopackage_utils import GeoPackageUtils
 from ..user_communication import UserCommunication
 from qgis.PyQt.QtWidgets import QProgressDialog
-
-try:
-    import h5py
-except ImportError:
-    pass
 
 
 class ASCProcessor(object):
@@ -66,10 +56,19 @@ class ASCProcessor(object):
 class NetCDFProcessor:
     def __init__(self, vlayer, nc_file, iface, gutils):
         self.vlayer = vlayer
+        self.uc = UserCommunication(iface, "FLO-2D")
+
+        try:
+            from netCDF4 import Dataset, num2date
+        except ImportError:
+            msg = "The netCDF4 Python package is required to read NetCDF files. Please install it and try again."
+            self.uc.bar_error(msg)
+            self.uc.log_info(msg)
+            raise ImportError(msg)
+
         self.nc = Dataset(nc_file, "r")
         self.iface = iface
         self.gutils = gutils
-        self.uc = UserCommunication(iface, "FLO-2D")
 
         convertion = 1000 if self.gutils.get_cont_par("METRIC") == "1" else 39.37  # Convert m to mm or m to inches
 
@@ -226,11 +225,21 @@ class TIFProcessor(object):
 
 class HDFProcessor(object):
     def __init__(self, hdf_path, iface):
+        self.uc = UserCommunication(iface, "FLO-2D")
+
+        try:
+            import h5py
+        except ImportError:
+            msg = "The h5py Python package is required to read HDF5 files. Please, install it and try again."
+            self.uc.bar_error(msg)
+            self.uc.log_info(msg)
+            raise ImportError(msg)
+
         self.iface = iface
         self.con = None
         self.gutils = None
         self.hdf_path = hdf_path
-        self.uc = UserCommunication(iface, "FLO-2D")
+
 
     def export_rainfall_to_binary_hdf5(self, header, qry_data, qry_size, qry_timeinterval, subdomain):
 
