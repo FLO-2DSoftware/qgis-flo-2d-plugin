ce5dbc614307aad19de785dfa2b91b94629e6601
rpachaly92 <robson@flo-2d.com>
2025-10-10 11:36:23 -0300

    Fix text formatting in rain editor and update plot item visibility in struct editor

--- flo2d/gui/struct_editor_widget.py
+++ flo2d/gui/struct_editor_widget.py
@@ -893,7 +893,7 @@ class StructEditorWidget(qtBaseClass, uiDialog):
                                            col=SCENARIO_COLOURS[i], sty=SCENARIO_STYLES[0])
                     else:
                         self.plot.add_item(f"{key} - Inflow ({self.system_units[units][2]})", [value['Time'], value['Inflow']],
-                                           col=SCENARIO_COLOURS[i], sty=SCENARIO_STYLES[0], hide=True)
+                                           col=SCENARIO_COLOURS[i], sty=SCENARIO_STYLES[0])
                     self.plot.add_item(f"{key} - Outflow ({self.system_units[units][2]})", [value['Time'], value['Outflow']],
                                        col=SCENARIO_COLOURS[i], sty=SCENARIO_STYLES[1], hide=True)
 
