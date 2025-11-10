c3d50138239086ca56a139a88e7e93a4612b4208
Cornelius Kibet <kibetcornelius56@gmail.com>
2025-11-10 22:14:12 +0300

    Update h5py import

--- flo2d/gui/dlg_project_review_scenarios.py
+++ flo2d/gui/dlg_project_review_scenarios.py
@@ -9,8 +9,8 @@ from ..flo2dobjects import ChannelSegment
 
 try:
     import h5py
-except ImportError:
-    pass
+except Exception as e:
+    raise ImportError("h5py could not be imported in the QGIS Python environment") from e
 from PyQt5.QtCore import QSettings, Qt
 from PyQt5.QtWidgets import QFileDialog, QProgressDialog
 
