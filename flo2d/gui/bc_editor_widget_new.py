df56c5e4f82f404d3ed7caedc46912b11a2cd2c6
Cornelius Kibet <kibetcornelius56@gmail.com>
2026-04-23 17:30:56 +0300

    Delete the following line as it is not used: grid = self.lyrs.data["grid"]["qlyr"]

--- flo2d/gui/bc_editor_widget_new.py
+++ flo2d/gui/bc_editor_widget_new.py
@@ -358,7 +358,6 @@ class BCEditorWidgetNew(qtBaseClass, uiDialog):
                 self.canvas.scene().removeItem(rb)
                 del rb
 
-            grid = self.lyrs.data["grid"]["qlyr"]
             lyr = self.lyrs.get_layer_by_name("Grid", group=self.lyrs.group).layer()
             gt = lyr.geometryType()
 
