e90b9df97e52b64069fdbd45ca95f555c5ae44c6
Cornelius Kibet <kibetcornelius56@gmail.com>
2026-04-29 15:35:03 +0300

    Ensure the dialog "Hazus Flooding Analysis" is parented to QGIS

--- flo2d/gui/dlg_hazus.py
+++ flo2d/gui/dlg_hazus.py
@@ -20,17 +20,18 @@ from ..gui.dlg_sampling_buildings_elevations import SamplingBuildingsElevationsD
 from ..gui.dlg_sampling_elev import SamplingElevDialog
 from ..user_communication import UserCommunication
 from .ui_utils import load_ui, set_icon
-from ..utils import qt_cursor_shape, qdialogbuttonbox_button
+from ..utils import qt_cursor_shape, qdialogbuttonbox_button, qt_window_flag
 
 uiDialog, qtBaseClass = load_ui("hazus")
 
 
 class HazusDialog(qtBaseClass, uiDialog):
     def __init__(self, con, iface, lyrs):
-        qtBaseClass.__init__(self)
+        qtBaseClass.__init__(self, iface.mainWindow())
         uiDialog.__init__(self)
         self.iface = iface
         self.setupUi(self)
+        self.setWindowFlags(qt_window_flag("Dialog") | qt_window_flag("Tool"))
         self.con = con
         self.lyrs = lyrs
         self.uc = UserCommunication(iface, "FLO-2D")
