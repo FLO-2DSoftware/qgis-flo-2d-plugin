c4beecca7b66564bb327290a655126f8a768bbf9
Robson Pachaly <39889306+rpachaly@users.noreply.github.com>
2026-06-03 13:29:02 -0300

    Refactor levee elevation tool dialog and enhance duplicate handling (#2149)

--- flo2d/gui/dlg_levee_elev.py
+++ flo2d/gui/dlg_levee_elev.py
@@ -8,18 +8,26 @@
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version
 import os
+import time
 import traceback
+from datetime import datetime
 
 from qgis.core import QgsFeature, QgsGeometry, QgsPointXY
 from qgis.PyQt.QtCore import QSettings
-from qgis.PyQt.QtWidgets import QApplication, QDialogButtonBox, QFileDialog
+from qgis.PyQt.QtWidgets import QApplication, QDialogButtonBox, QFileDialog, QMessageBox
 
 from ..flo2d_tools.elevation_correctors import LeveesElevation
+from ..flo2d_tools.grid_tools import cellIDNumpyArray, dirID
 from ..geopackage_utils import GeoPackageUtils
 from ..gui.dlg_walls_shapefile import WallsShapefile
 from ..user_communication import UserCommunication
 from .ui_utils import load_ui
-from ..utils import qdialogbuttonbox_button, qt_window_flag
+from ..utils import qdialogbuttonbox_button, qt_window_flag, qt_cursor_shape
+
+from ..flo2d_tools.schematic_tools import (
+    delete_redundant_levee_directions_np,
+    generate_schematic_levees,
+)
 
 uiDialog, qtBaseClass = load_ui("levees_elevation")
 
@@ -30,7 +38,12 @@ class LeveesToolDialog(qtBaseClass, uiDialog):
         uiDialog.__init__(self)
         self.iface = iface
         self.setupUi(self)
-        self.setWindowFlags(qt_window_flag("Dialog") | qt_window_flag("Tool"))
+        self.setWindowFlags(
+            qt_window_flag("Window") |
+            qt_window_flag("WindowMinimizeButtonHint") |
+            qt_window_flag("WindowCloseButtonHint") |
+            qt_window_flag("WindowSystemMenuHint")
+        )
         self.con = con
         self.lyrs = lyrs
         self.uc = UserCommunication(iface, "FLO-2D")
@@ -40,6 +53,7 @@ class LeveesToolDialog(qtBaseClass, uiDialog):
         self.methods = {}
 
         self.levees_tool_buttonBox.button(qdialogbuttonbox_button("Ok")).setText("Create Schematic Layers from User Levees")
+
         # connections
         self.elev_polygons_chbox.stateChanged.connect(self.polygons_checked)
         self.elev_points_chbox.stateChanged.connect(self.points_checked)
@@ -113,6 +127,278 @@ class LeveesToolDialog(qtBaseClass, uiDialog):
             self.uc.show_warn("WARNING 060319.1612: Please choose at least one crest elevation source!")
             self.uc.log_info("WARNING 060319.1612: Please choose at least one crest elevation source!")
             return False
+
+        try:
+            start = datetime.now()
+            QApplication.setOverrideCursor(qt_cursor_shape("WaitCursor"))
+            n_elements_total = 1
+            n_levee_directions_total = 0
+            n_fail_features_total = 0
+
+            starttime = time.time()
+            levees = self.lyrs.data["levee_data"]["qlyr"]
+
+            # This for loop creates the attributes in the levee_dat
+            for (
+                    n_elements,
+                    n_levee_directions,
+                    n_fail_features,
+                    ranger,
+            ) in self.schematize_levees():
+                n_elements_total += n_elements
+                n_levee_directions_total += n_levee_directions
+                n_fail_features_total += n_fail_features
+
+            # This for loop corrects the elevation
+            for no in sorted(self.methods):
+                self.methods[no]()
+
+            inctime = time.time()
+            self.uc.log_info("%s seconds to process levee features" % round(inctime - starttime, 2))
+
+            # Delete duplicates:
+            q = False
+            if n_elements_total > 0:
+                dletes = "Cell - Direction\n---------------\n"
+
+                # delete duplicate elements with the same direction and elevation too
+                qryIndex = "CREATE INDEX if not exists levee_dataFIDGRIDFIDLDIRLEVCEST  ON levee_data (fid, grid_fid, ldir, levcrest);"
+                self.gutils.con.execute(qryIndex)
+                self.gutils.con.commit()
+
+                # levees_dup_qry = "SELECT min(fid), grid_fid, ldir, levcrest FROM levee_data GROUP BY grid_fid, ldir, levcrest HAVING COUNT(ldir) > 1 and count(levcrest) > 1 ORDER BY grid_fid"
+                levees_dup_qry = "SELECT fid, grid_fid, ldir, max(levcrest) FROM levee_data GROUP BY grid_fid, ldir HAVING COUNT(grid_fid) = 2"
+
+                leveeDups = self.gutils.execute(levees_dup_qry).fetchall()  # min FID, grid fid, ldir, min levcrest
+                # grab the values
+                self.uc.log_info(
+                    "Found {valer} levee elements with duplicated grid, ldir, and elev; deleting the duplicates;".format(
+                        valer=len(leveeDups)
+                    )
+                )
+
+                delete_fids = []
+
+                for item in leveeDups:
+                    delete_fids.append(item[0])
+
+                # delete any duplicates in directions that aren't the min elevation
+                for fid in delete_fids:
+                    self.gutils.execute(f"DELETE FROM levee_data WHERE fid = {fid};")
+                # levees_dup_delete_qry =
+                #     "DELETE FROM levee_data WHERE fid = ?;"
+                # )
+                # self.gutils.con.executemany(levees_dup_delete_qry, del_dup_data)
+                # self.gutils.con.commit()
+
+                qryIndexDrop = "DROP INDEX if exists levee_dataFIDGRIDFIDLDIRLEVCEST;"
+                self.gutils.con.execute(qryIndexDrop)
+                self.gutils.con.commit()
+
+                leveesToDelete = delete_redundant_levee_directions_np(
+                    self.gutils, cellIDNumpyArray
+                )  # pass grid layer if it exists
+                # leveesToDelete = delete_levee_directions_duplicates(self.gutils, levees, grid_lyr)
+                if len(leveesToDelete) > 0:
+                    k = 0
+                    i = 0
+                    for levee in leveesToDelete:
+                        k += 1
+
+                        i += 1
+
+                        if i < 50:
+                            if k <= 3:
+                                dletes += (
+                                        "{:<25}".format(
+                                            "{:>10}-{:1}({:2})".format(
+                                                str(levee[0]),
+                                                str(levee[1]),
+                                                dirID(levee[1]),
+                                            )
+                                        )
+                                        + "\t"
+                                )
+                            elif k == 4:
+                                dletes += "{:<25}".format(
+                                    "{:>10}-{:1}({:2})".format(str(levee[0]), str(levee[1]), dirID(levee[1]))
+                                )
+                            elif k > 4:
+                                dletes += (
+                                        "\n"
+                                        + "{:<25}".format(
+                                    "{:>10}-{:1}({:2})".format(
+                                        str(levee[0]),
+                                        str(levee[1]),
+                                        dirID(levee[1]),
+                                    )
+                                )
+                                        + "\t"
+                                )
+                                k = 1
+
+                        else:
+                            if k <= 3:
+                                dletes += (
+                                        "{:<25}".format(
+                                            "{:>10}-{:1}({:2})".format(
+                                                str(levee[0]),
+                                                str(levee[1]),
+                                                dirID(levee[1]),
+                                            )
+                                        )
+                                        + "\t"
+                                )
+                            elif k == 4:
+                                dletes += "{:<25}".format(
+                                    "{:>10}-{:1}({:2})".format(str(levee[0]), str(levee[1]), dirID(levee[1]))
+                                )
+                            elif k > 4:
+                                dletes += (
+                                        "\n"
+                                        + "{:<25}".format(
+                                    "{:>10}-{:1}({:2})".format(
+                                        str(levee[0]),
+                                        str(levee[1]),
+                                        dirID(levee[1]),
+                                    )
+                                )
+                                        + "\t"
+                                )
+                                k = 1
+
+                    dletes += "\n\nWould you like to delete them?"
+
+                    #                     dletes = Qt.convertFromPlainText(dletes)
+                    QApplication.restoreOverrideCursor()
+
+                    parent = self.iface.mainWindow() if self.iface and self.iface.mainWindow() else None
+                    m = QMessageBox(parent)
+                    title = "Duplicate Opposite Levee Directions".center(170)
+                    m.setWindowTitle(title)
+                    m.setText(
+                        "There are "
+                        + str(len(leveesToDelete))
+                        + " redundant levees directions. "
+                        + "They have lower crest elevation than the opposite direction.\n\n"
+                        + "Would you like to delete them?"
+                    )
+                    m.setDetailedText(dletes)
+                    m.setStandardButtons(self.uc.msgbox_button("No") | self.uc.msgbox_button("Yes"))
+                    m.setDefaultButton(self.uc.msgbox_button("Yes"))
+
+                    # Spacer                        width, height, h policy, v policy
+                    # horizontalSpacer = QSpacerItem(0, 300, QSizePolicy.Preferred, QSizePolicy.Preferred)
+                    #                     verticalSpacer = QSpacerItem(10, 10, QSizePolicy.Expanding, QSizePolicy.Expanding)
+                    layout = m.layout()
+                    # layout.addItem(horizontalSpacer)
+                    #                     layout.addItem(verticalSpacer)
+
+                    #                     m.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding);
+                    #                     m.setSizePolicy(QSizePolicy.Maximum,QSizePolicy.Maximum);
+                    #                     m.setFixedHeight(12000);
+                    #                     m.setFixedWidth(12000);
+
+                    #                     m.setFixedSize(2000, 1000);
+                    #                     m.setBaseSize(QSize(2000, 1000))
+                    #                     m.setMinimumSize(1000,1000)
+
+                    #                     m.setInformativeText(dletes + '\n\nWould you like to delete them?')
+                    q = m.exec()
+                    if q == self.uc.msgbox_button("Yes"):
+                        #                     q = self.uc.question('The following are ' + str(len(leveesToDelete)) + ' opposite levees directions duplicated (with lower crest elevation).\n' +
+                        #                                             'Would you like to delete them?\n\n' + dletes + '\n\nWould you like to delete them?')
+                        #                     if q:
+                        delete_levees_qry = """DELETE FROM levee_data WHERE grid_fid = ? AND ldir = ?;"""
+                        delete_failure_qry = """DELETE FROM levee_failure WHERE grid_fid = ? and lfaildir = ?;"""
+                        self.uc.log_info("Deleting extra levee and levee failure features")
+
+                        # build indexes to speed up the process
+                        qryIndex = (
+                            """CREATE INDEX if not exists leveeDataGridFID_LDIR  ON levee_data (grid_fid, ldir);"""
+                        )
+                        self.gutils.execute(qryIndex)
+                        qryIndex = """CREATE INDEX if not exists leveeFailureGridFID_LFAILDIR  ON levee_failure (grid_fid, lfaildir);"""
+                        self.gutils.execute(qryIndex)
+                        self.gutils.con.commit()
+
+                        # cur = self.gutils.con.cursor()
+                        # cur.executemany(delete_levees_qry, list([(str(levee[0]), str(levee[1]),) for levee in leveesToDelete]))
+                        # self.gutils.con.commit()
+                        # cur.executemany(delete_failure_qry, list([(str(levee[0]), str(levee[1]),) for levee in leveesToDelete]))
+                        # self.gutils.con.commit()
+                        # cur.close()
+
+                        for leveeCounter, levee in enumerate(leveesToDelete):
+                            # self.gutils.execute(delete_levees_qry, (levee[0], levee[1]))
+                            self.gutils.execute(
+                                "DELETE FROM levee_data WHERE grid_fid = %i AND ldir = %i;" % (levee[0], levee[1])
+                            )
+                            if leveeCounter % 1000 == 0:
+                                print(
+                                    "DELETE FROM levee_data WHERE grid_fid = %i AND ldir = %i;" % (levee[0], levee[1])
+                                )
+                            self.gutils.con.commit()
+                            # self.gutils.execute(delete_failure_qry, (levee[0], levee[1]))
+                            self.gutils.execute(
+                                "DELETE FROM levee_failure WHERE grid_fid = %i and lfaildir = %i;"
+                                % (levee[0], levee[1])
+                            )
+                            if leveeCounter % 1000 == 0:
+                                print(
+                                    "DELETE FROM levee_failure WHERE grid_fid = %i and lfaildir = %i;"
+                                    % (levee[0], levee[1])
+                                )
+                            self.gutils.con.commit()
+                        self.uc.log_info("Done deleting extra levee and levee failure features")
+                        qryIndex = """DROP INDEX if exists leveeDataGridFID_LDIR;"""
+                        self.gutils.execute(qryIndex)
+                        qryIndex = """DROP INDEX if exists leveeFailureGridFID_LFAILDIR;"""
+                        self.gutils.execute(qryIndex)
+                        self.gutils.con.commit()
+
+                        levees.triggerRepaint()
+
+                levee_schem = self.lyrs.get_layer_by_name("Levees", group=self.lyrs.group).layer()
+                if levee_schem:
+                    levee_schem.triggerRepaint()
+            if q:
+                n_levee_directions_total -= len(leveesToDelete)
+                n_fail_features_total -= len(leveesToDelete)
+                if n_fail_features_total < 0:
+                    n_fail_features_total = 0
+
+            #             end = datetime.now()
+            #             time_taken = end - start
+            #             self.uc.show_info("Time to schematize levee cells. " + str(time_taken))
+
+            levees = self.lyrs.data["levee_data"]["qlyr"]
+            idx = levees.fields().indexOf("grid_fid")
+            values = levees.uniqueValues(idx)
+            QApplication.restoreOverrideCursor()
+            info = (
+                    "Values assigned to the Schematic Levees layer!"
+                    + "\n\nThere are now "
+                    + str(len(values))
+                    + " grid elements with levees,"
+                    + "\nwith "
+                    + str(n_levee_directions_total)
+                    + " levee directions,"
+                    + "\nof which, "
+                    + str(n_fail_features_total)
+                    + " have failure data."
+            )
+            if n_fail_features_total > n_levee_directions_total:
+                info += "\n\n(WARNING 191219.1649: Please review the input User Levee Lines. There may be more than one line intersecting grid elements)"
+            self.uc.show_info(info)
+
+        except Exception as e:
+            QApplication.restoreOverrideCursor()
+            self.uc.log_info(traceback.format_exc())
+            self.uc.show_error(
+                "ERROR 060319.1806: Assigning values aborted! Please check your crest elevation source layers.\n",
+                e,
+            )
 
     def import_z_data(self):
         elev_points_lyr = self.lyrs.data["user_elevation_points"]["qlyr"]
@@ -201,3 +487,22 @@ class LeveesToolDialog(qtBaseClass, uiDialog):
             self.corrector.elevation_from_polygons()
         finally:
             self.corrector.clear_filter()
+
+    def schematize_levees(self):
+        """
+        Generate schematic lines for user defined levee lines.
+        """
+        try:
+            levee_lyr = self.lyrs.get_layer_by_name("Levee Lines", group=self.lyrs.group).layer()
+            grid_lyr = self.lyrs.get_layer_by_name("Grid", group=self.lyrs.group).layer()
+
+            for (
+                    n_elements,
+                    n_levee_directions,
+                    n_fail_features,
+                    regionReq,
+            ) in generate_schematic_levees(self.gutils, levee_lyr, grid_lyr):
+                yield (n_elements, n_levee_directions, n_fail_features, regionReq)
+        except Exception as e:
+            QApplication.restoreOverrideCursor()
+            self.uc.show_error("ERROR 030120.0723: unable to process user levees!\n", e)
